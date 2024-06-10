9dcdd5d9e93845977641bf78b4405a9ccd206dca
18ho <165611879+18ho@users.noreply.github.com>
2024-06-10 17:00:46 +0900

    Delete project directory

--- project/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // 모든 세션 변수 제거
-session_destroy(); // 세션 파기
-echo "<script>
-        alert('이용해 주셔서 감사합니다.');
-        window.location.href = 'login.php';
-      </script>";
-exit();
